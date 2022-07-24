e4eb98b3a87c17a5753d90bdd102bc00518d85a4
diewland <diewland@gmail.com>
2022-07-24 16:50:45 +0700

    add gen-all script

---
+++ gen_all.sh
@@ -0,0 +1,30 @@
+# nerdape
+echo 'nerdape -> 20220716.csv'
+cd nerdape
+python3 gen_csv.py 20220710.csv > 20220716.csv
+cd ..
+# kasou-shin
+echo 'kasou-shin -> 20220716.csv'
+cd kasou-shin
+python3 gen_csv.py > 20220716.csv
+cd ..
+# holo-mask-bw
+echo 'holo-mask-bw -> 20220716.csv'
+cd holo-mask-bw
+python3 gen_csv.py > 20220716.csv
+cd ..
+# holo-mask-blue
+echo 'holo-mask-blue -> 20220716.csv'
+cd holo-mask-blue
+python3 gen_csv.py > 20220716.csv
+cd ..
+# olien
+echo 'olien -> 20220716.csv'
+cd olien
+python3 gen_csv.py > 20220716.csv
+cd ..
+# holo-mask-gold
+echo 'holo-mask-gold -> 20220717.csv'
+cd holo-mask-gold
+python3 gen_csv.py > 20220717.csv
+cd ..
