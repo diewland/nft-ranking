9691f89fa3c2910fba9de19d7f70c1480c5e0b5a
diewland <diewland@gmail.com>
2022-10-03 20:42:28 +0700

    udpate data

--- sobta-magic/json/download.py
+++ sobta-magic/json/download.py
@@ -3,8 +3,8 @@ from pathlib import Path
 sys.path.append(str(Path('.').absolute().parent.parent))
 from smart_download import *
 
-URL_DOWNLOAD = "https://cloudflare-ipfs.com/ipfs/bafybeifuownwzsuifhg5xajhfnbdtyugblnztxwnaf33ixetjxy4ktzcyu/{}.json"
+URL_DOWNLOAD = "https://cloudflare-ipfs.com/ipfs/bafybeic7pphnmkwee4p4vw7f47c2faw42gtksevak2i7bxahbmlr4ddy6y/{}.json"
 FROM_ID = 1
-TO_ID = 559
+TO_ID = 806
 
 smart_download(URL_DOWNLOAD, FROM_ID, TO_ID)
